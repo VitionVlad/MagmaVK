33d26168177f26581a72df70546204d9c340849a
vition vlad <48290199+VitionVlad@users.noreply.github.com>
2022-07-21 13:41:28 +0300

    Update Reader.hpp

--- Reader.hpp
+++ Reader.hpp
@@ -18,3 +18,19 @@ static std::vector<char> loadbin(const std::string& filename) {
 	file.close();
 	return buffer;
 }
+
+void readImage(unsigned char* pixels, int &resolutionx, int &resolutiony, const char* path){ //ppm ascii
+    fstream readimage;
+    readimage.open(path);
+    int i1, i2, i3;
+    string trash;
+    readimage >> trash;
+    readimage >> resolutionx >> resolutiony;
+    readimage >> i1;
+    for(int i = 0; readimage >> i1 >> i2 >> i3; i+=4){
+        pixels[i] = i1;
+        pixels[i+1] = i2;
+        pixels[i+2] = i3;
+        pixels[i+3] = 255;
+    }
+}
